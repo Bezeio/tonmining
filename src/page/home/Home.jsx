4c9c09319c3c909056966e077855fc3ebd8a0287
Bezeio <phong280201@gmail.com>
2024-08-03 02:27:20 +0700

    up

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,38 +1,45 @@
+import React from "react";
 import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <main className="main-content">
-        <h1 className="main-title">
-          Don't just mine,
-          <br />
-          mineTON!
-        </h1>
+    <>
+      <div className="overlay"></div>
+      <div className="App">
+        <div className="home-container">
+          <main className="main-content">
+            <h1 className="main-title">
+              Don't just mine,
+              <br />
+              mineTON!
+            </h1>
 
-        <p className="description">
-          The first DEPin on TON. Cloud mining made simple. Mine-to-earn model.
-          <br />
-          Every tap contributes to profit-sharing and endless excitement.
-        </p>
-        <div className="cta-buttons">
-          <a
-            href="https://t.me/tntGPUBot/tonmining"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button className="launch-app-btn">Launch App</button>
-          </a>
-          <a
-            href="https://t.me/tntGPUBot/tonmining"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button className="check-airdrop-btn">Check Your Airdrop</button>
-          </a>
+            <p className="description">
+              The first DEPin on TON. Cloud mining made simple. Mine-to-earn model.
+              <br />
+              Every tap contributes to profit-sharing and endless excitement.
+            </p>
+            <div className="cta-buttons">
+              <a
+                href="https://t.me/tntGPUBot/tonmining"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="launch-app-btn">Launch App</button>
+              </a>
+              <a
+                href="https://t.me/tntGPUBot/tonmining"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="check-airdrop-btn">Check Your Airdrop</button>
+              </a>
+            </div>
+          </main>
         </div>
-      </main>
-    </div>
+      </div>
+    </>
   );
 };
+
 export default Home;
